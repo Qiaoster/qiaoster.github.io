24cca473fb1ea9ef6c82edbf422dcd7ce3ad579d
Qiao <whateverzimu@gmail.com>
2021-11-21 22:59:17 +0800

    21

---
+++ 2021-11-21 Flight Among Stars.js
@@ -0,0 +1,78 @@
+class Star {
+  reset() {
+    this.x = int(random(10,width) - halfWidth);
+    this.y = int(random(10,height) - halfHeight);
+    this.z = random(0.1,1.5);
+    this.h = random(0,255);
+    this.s = random(4,40);
+    this.b = (this.z / 1.4 + 0.1) * 255;
+  }
+  
+  constructor() {
+    this.reset();
+  }
+}
+
+function CopyStar(star) {
+  var newStar = new Star();
+  newStar.x = star.x;
+  newStar.y = star.y;
+  newStar.z = star.z;
+  newStar.h = star.h;
+  newStar.s = star.s;
+  newStar.b = star.b;
+  
+  return newStar;
+}
+
+var nStars = 130;
+var stars = [];
+var oldStars = [];
+var halfWidth;
+var halfHeight;
+
+function setup() {
+  createCanvas(300,300);
+  halfWidth = width/2;
+  halfHeight = height/2;
+  for (let i = 0; i < nStars; ++i)
+  {
+    stars.push(new Star());
+    oldStars.push(CopyStar(stars[i]));
+  }
+  
+  colorMode(HSB, 100);
+}
+
+function draw() {
+  colorMode(HSB, 100);
+  let h = random(0,255);
+  let s = random(10,20);
+  let b = random(0,25);
+  background(h,s,b);
+  translate(halfWidth + random(-3,3), halfHeight + random(-3,3));
+  for (let i = 0; i < nStars; ++i)
+  {
+    let sx = oldStars[i].x * oldStars[i].z;
+    let sy = oldStars[i].y * oldStars[i].z;
+    strokeWeight(min(oldStars[i].z * 2.5, 45));
+    stroke(oldStars[i].h, oldStars[i].s, oldStars[i].b);
+    point(sx,sy);
+  }
+  
+  for (let i = 0; i < nStars; ++i)
+  {
+    stars[i].z *= 1 + stars[i].z/27;
+    let sx = stars[i].x * stars[i].z;
+    let sy = stars[i].y * stars[i].z;
+    strokeWeight(min(stars[i].z * 2.5, 45));
+    this.b = min((stars[i].z + 1) * this.b, 255);
+    stroke(stars[i].h, stars[i].s, stars[i].b);
+    point(sx,sy);
+    if (sx < -halfWidth || sx > halfWidth || sy < -halfHeight || sy > halfHeight)
+    {
+      stars[i].reset();
+    }
+    oldStars[i] = CopyStar(stars[i]);
+  }
+}
